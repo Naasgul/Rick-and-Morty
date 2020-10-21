0e3bdeb8f77fd7d9ee79035cb1cec092b1014d25
Naasgul <lnaas@hotmail.de>
2020-10-21 15:44:14 +0200

    Add function to display all/20 characters

--- src/App.js
+++ src/App.js
@@ -2,30 +2,42 @@ import "./app.css";
 import Header from "./components/Header";
 import { createElement } from "./utils/elements";
 import Character from "./components/Character";
-import { getCharacterById } from "./utils/api";
+import { getAllCharacters } from "./utils/api";
 
 function App() {
   const header = Header();
-  const main = createElement("main");
+  const characterContainer = createElement("section", {
+    className: "allCharacterCards",
+  });
+  const searchField = createElement("input", {
+    className: "searchField",
+    type: "search",
+    placeholder: "Search Rick and Morty Database",
+  });
+  const main = createElement("main", {
+    children: [searchField, characterContainer],
+    className: "main",
+  });
 
-  async function getCharacters(id) {
-    const character = await getCharacterById(id);
-
-    main.append(
+  async function allCharacters() {
+    const characters = await getAllCharacters();
+    const characterElements = characters.map((character) =>
       Character({
         name: character.name,
         imgSrc: character.image,
       })
     );
+    characterContainer.append(...characterElements);
   }
+  allCharacters();
 
   /*  const IDs = [1, 2, 3, 4, 5, 6];
 
   IDs.forEach(getCharacters); */
 
-  for (let i = 1; i <= 15; i++) {
+  /*  for (let i = 1; i <= 15; i++) {
     getCharacters(i);
-  }
+  } */
 
   const container = createElement("div", {
     children: [header, main],
